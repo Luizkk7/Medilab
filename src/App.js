acf0ce019ef6971f0bea4e648b9d0eb7b2f7a312
Luiz Andrade <100077148+Luizkk7@users.noreply.github.com>
2023-02-13 14:02:05 -0300

    alteracoes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import Clientes from "./components/Clientes";
 import Sobre from "./components/Sobre";
 import { BrowserRouter, Routes, Link, Route } from "react-router-dom";
 import { Nav } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import img1 from "./components/img/img1.jpeg";
+import Navbar from 'react-bootstrap/Navbar';
+import "./App.css";
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Medilab</h1>
+      
       <BrowserRouter>
-        <Nav variant="tabs">
+      <Navbar bg="dark" variant={"dark"} expand="lg">
+      <img
+        src={img1}
+        id="logo"
+        width="40px"
+        height="43px"
+        alt="Logotipo da aplicação"
+      />
+                        <Navbar.Brand href="#"> Medilab Sistemas</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                        <Nav variant="tabs">
           <Nav.Link as={Link} to="/">
             {" "}
             Pagína Inicial
@@ -24,6 +40,9 @@ function App() {
             Sobre
           </Nav.Link>
         </Nav>
+
+                        </Navbar.Collapse>
+                    </Navbar>
 
         <Routes>
           <Route path="/" element={<Home />}></Route>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,52 @@
+import Carousel from 'react-bootstrap/Carousel';
+import img2 from "./img/img2.jpg"
+import img3 from "./img/img3.jpg"
+import img4 from "./img/img4.jpg"
+
 function Home() {
   return (
     <div>
-      <p>Esta é a pagina principal</p>
+        <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={img2}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Medilab</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={img3}
+          alt="First slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Medilab</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={img4}
+          alt="First slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+      
     </div>
   );
 }
